dda09ebf245fe339939a4e2dbcaf4c7f903b0852
Takeshi Ishita <takeshi.ishita@dena.jp>
2018-07-26 20:14:13 +0900

    Add a function for annotating points. Specify font and point colors

--- run_bunny_reconstruction.py
+++ run_bunny_reconstruction.py
@@ -1,6 +1,9 @@
 import sys
 
 import numpy as np
+
+from matplotlib.animation import FuncAnimation
+from matplotlib.font_manager import FontProperties
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -19,29 +22,48 @@ def read_object(filename):
     return np.vstack((x, y, z))
 
 
-def plot2d(X):
+def annotate(ax, P, labels=None):
+    if labels is None:
+        labels = range(len(P))
+
+    font = FontProperties()
+    font.set_weight("bold")
+
+    for label, p in zip(labels, P):
+        ax.text(*p, label, alpha=0.8, fontproperties=font)
+
+
+def plot2d(P, do_annotate=False, color=None):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(X[0], X[1], '.')
+
+    ax.scatter(P[:, 0], P[:, 1], c=color)
+
+    if do_annotate:
+        annotate(ax, P, labels=range(0, len(P), 50))
+
     ax.set_xlabel('x axis')
     ax.set_ylabel('y axis')
     ax.set_aspect('equal', 'datalim')
+
     plt.show()
 
 
-def plot3d(*points):
+def plot3d(P, do_annotate=False, elev=45, azim=0, color=None):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    for i, P in enumerate(points):
-        ax.plot(P[0], P[1], P[2], '.', label=str(i))
-        ax.set_xlabel('x axis')
-        ax.set_ylabel('y axis')
-        ax.set_zlabel('z axis')
-        ax.legend()
+    ax.scatter(P[:, 0], P[:, 1], P[:, 2], c=color)
 
-    ax.view_init(elev=135, azim=90)
+    if do_annotate:
+        annotate(ax, P)
+
+    ax.set_xlabel('x axis')
+    ax.set_ylabel('y axis')
+    ax.set_zlabel('z axis')
+    ax.view_init(elev, azim)
     ax.set_aspect('equal', 'datalim')
+
     plt.show()
 
 
