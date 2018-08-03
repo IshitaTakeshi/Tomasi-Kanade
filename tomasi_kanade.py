f0f0f0d46628c8d4476f981f9d4348b66075345b
Takeshi Ishita <takeshi.ishita@dena.jp>
2018-08-03 19:21:05 +0900

    Add docstrings

--- tomasi_kanade.py
+++ tomasi_kanade.py
@@ -8,19 +8,28 @@ class TomasiKanade(object):
     The main process of the Tomasi-Kanade method
 
     Args:
-        X_eval: 3D point cloud which will be used to evaluate
+        X_eval (np.ndarray): 3D point cloud which will be used to evaluate
             the reconstructed model
+        learning_rate (float): Hyperparameter used in the affine correction
+            which run in the reconstruction process
     """
 
     def __init__(self, X_eval: np.ndarray = None):
         self.affine_correction = AffineCorrection(X_eval)
         self.image_points = []
 
-    def add_image_points(self, image_points):
-        self.image_points.append(image_points)
+    def add_image_points(self, image_points: np.ndarray):
+        """
+        Add 2D image points to form a measurement matrix
+
+        Args:
+            image_points: Image points of shape (n_points, 2)
+        """
+        self.image_points.append(image_points.T)
 
     @property
     def measurement_matrix(self):
+        """Measurement matrix of shape (2 * n_views, n_points)"""
         return np.vstack(self.image_points)
 
     def run(self):
